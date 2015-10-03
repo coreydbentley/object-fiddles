5907b5ddfecbda5ef7a9e3aa6bc7dda30c9536e8
coreydbentley <coreydbentley@gmail.com>
2015-10-03 10:47:22 -0600

    added states to object-fiddels

--- practice.js
+++ practice.js
@@ -1,9 +1,15 @@
 //Once you complete a problem, open up Chrome and check the answer in the console.
 
 
-//Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
+//Create an object called me. Give it a key of name with the value being your name, 
+//and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
+  var me = {
+    name: "Corey",
+    age: 36
+  }
+
+  alert("Name: " me.name + "Age: " + me.age);
 
 
 
@@ -13,19 +19,28 @@
 
 
 
-//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
+//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday.
+// Have the values to those keys be your favorite thing in that category.
 
-  //Code here
+  var favoriteThings = {
+    band: "BTNH",
+    food: "Pasta",
+    person: "Amanda",
+    book: "Non-Fiction",
+    movie: "Shawshank Redemption",
+    holiday: "Christmas"
+  }
 
+//After you've made your object, add another key named 'car' with the value being your 
+//favorite car and then another key named 'brand' with the value being your favorite brand.
 
-//After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
+  favoriteThings.car = "Running";
 
-  //Code here
+//Now change the food key in your favoriteThings object to be 'Lettuce' 
+//and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
-
-//Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
-
-  //Code here
+  favoriteThings.food = 'Lettuce';
+  favoriteThings.book = '50 Shades of Grey';
 
 
 
@@ -42,18 +57,21 @@ Set the value of that key to 'chapstick'.
 Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. */
 
-  //Code here
+  var backPack = {};
+  var item = "firstPocket";
+  backPack["item"];
+  backPack.firstPocket = "chapstick";
+
 
 //After you do the above, alert your entire backPack object.
 
-  //Code here
+  alert(backPack);
 
 /*You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. */
 
-  //Code here
-
+  console.log(backPack);
 
 
 
@@ -62,13 +80,25 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 
 
-//Create another 'me' object with the following properties name, age, height, gender, married, eyeColor, hairColor. Fill those properties in with the appropriate values.
+//Create another 'me' object with the following properties 
+//name, age, height, gender, married, eyeColor, hairColor. 
+//Fill those properties in with the appropriate values.
 
-  //Code Here
+  var me = {
+    name: "Corey",
+    age: "36",
+    height: "5'5.5\"",
+    gender: "Male",
+    married: "No",
+    eyeColor: "Blue",
+    hairColor: "Dark Blonde"
+  }
 
 //Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
 
-  //Code Here
+  for(prop in me){
+    alert(me[prop]);
+  }
 
 
 
@@ -78,13 +108,17 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 
 
-//Create an Object called 'album' with 5 keys named different song titles that you make up, with the values being the length of each song.
+//Create an Object called 'album' with 5 keys named different song titles that you make up,
+// with the values being the length of each song.
 
-  //Code Here
+  var album = {one: "5 min", two: "3 min", three: "4 min", four: "3 min", five: "5 min"};
+
 
 //Now, loop through your album object alerting every song title individually.
 
-  //Code Here
+  for(prop in album){
+    alert(prop);
+  }
 
 
 
@@ -94,13 +128,25 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 
 
-//Create an object called states that has 5 US states as properties with the values being their population (doesn't have to be accurate).
+//Create an object called states that has 5 US states as properties with the values being 
+//their population (doesn't have to be accurate).
 
-  //Code Here
+  var states = {
+    Utah: 200000,
+    Arizona: 3000,
+    Colorado: 6000000,
+    Florida: 800000,
+    California: 7000000
+  } 
 
-//Now, loop through your states object and if the states population is greater than 30K, alert that state.
+//Now, loop through your states object and if the states population is greater than 30K, 
+//alert that state.
 
-  //Code Here
+  for(prop in states){
+    if( states[prop] > 30000){
+      alert(prop);
+    }
+  }
 
 
 
